481e38b756cbdfb3d6f336166b501ea359ee2332
yosuke <king.crimson.723.yu@gmail.com>
2022-02-22 00:19:31 +0900

    add:マイグレーションファイルにテーブル定義を実装

--- database/migrations/2022_02_19_030241_create_memos_table.php
+++ database/migrations/2022_02_19_030241_create_memos_table.php
@@ -7,15 +7,23 @@ use Illuminate\Support\Facades\Schema;
 class CreateMemosTable extends Migration
 {
     /**
-     * Run the migrations.
+     * // マイグレーション時に実行されるメソッドでテーブルのカラムを定義
      *
      * @return void
      */
     public function up()
     {
         Schema::create('memos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('id', true); // 外部キー制約をするためunsignedで符号（±など）をつけない。第二引数は自動採番設定。
+            $table->longText('content');
+            $table->unsignedBigInteger('user_id');
+
+            // 論理削除を定義=>deleted_atを自動生成する。
+            $table->softDeletes();
+            // timestampと記載するとレコード挿入時、更新時に値が入らないので、DB::rawで直接記載している
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP')); // 更新時間をデフォルト値で入れる。
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP')); // 作成時間も同様
+            $table->foreign('user_id')->references('id')->on('users'); // usersテーブルとの外部キー制約
         });
     }
 
