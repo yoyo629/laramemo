54ed9a8c24a884344e38ee1aaaf2564418a916b7
yosuke <king.crimson.723.yu@gmail.com>
2022-02-24 01:44:50 +0900

    add:メモ一覧のレンダリング処理実装

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,9 +88,11 @@
                 </div>
                 <div class="col-md-4 p-0">
                     <div class="card">
-                        <div class="card-header">中央カラム</div>
+                        <div class="card-header">メモ一覧</div>
                         <div class="card-body">
-                            <p class="card-text">with supporting text below as a natural lead-in to additional content.</p>
+                    @foreach($memos as $memo)
+                            <a class="cart-text d-block">{{ $memo['content'] }}</a>
+                    @endforeach
                         </div>
                     </div>
                 </div>
